d4971cb8b17764f5fab60697c157ae4bb298aaeb
Liu Qun (liuqun) <qunliu@zyhx-group.com>
2017-09-25 17:29:21 +0800

    new: C++ class ApplicationResourceRecorder

---
+++ ApplicationResourceRecorder.cpp
@@ -0,0 +1,33 @@
+﻿/* encoding:utf8 */
+
+/* Copyright (c) 2017, 青岛中怡智能安全研究院有限公司
+ * All rights reserved.
+ */
+
+#include "ApplicationResourceRecorder.h"
+#include "config.h"
+
+void ApplicationResourceRecorder::registerInstance(instance_ptr_t instance, instance_destructor_func_t destructorFunc)
+{
+    item_t item;
+
+    item.destructorFunc = destructorFunc;
+    item.instance = instance;
+    m_items.push(item);
+}
+
+ApplicationResourceRecorder::ApplicationResourceRecorder()
+{
+}
+
+ApplicationResourceRecorder::~ApplicationResourceRecorder()
+{
+    while(!m_items.empty()) {
+        item_t& item = m_items.top();
+        m_items.pop();
+        if (!item.destructorFunc) {
+            continue;
+        }
+        item.destructorFunc(item.instance);
+    }
+}
---
+++ ApplicationResourceRecorder.h
@@ -0,0 +1,68 @@
+﻿/* encoding:utf8 */
+
+/* Copyright (c) 2017, 青岛中怡智能安全研究院有限公司
+ * All rights reserved.
+ */
+
+#ifndef APPLICATION_RESOURCE_RECORDER_H
+#define APPLICATION_RESOURCE_RECORDER_H
+
+#include <stack>
+#include "config.h"
+
+extern "C" {
+
+/// Pointing to an instance
+typedef void *instance_ptr_t;
+
+/// Destructor function type
+typedef void (*instance_destructor_func_t)(instance_ptr_t);
+
+/// A structure to hold an item
+typedef struct item_t {
+    instance_ptr_t instance;
+    instance_destructor_func_t destructorFunc;
+} item_t;
+
+};
+
+/// A collector to record dynamic allocated objects
+///
+/// At the end of the recorder's life-time, all registered objects will be destructed automatically.
+/// You do not need to call free() or fclose() on each item before exit(ERROR).
+///
+/// Usage:
+/// ```
+/// #include <stdlib.h>
+/// #include "ApplicationResourceRecorder.h"
+/// int main ()
+/// {
+///     ApplicationResourceRecorder recorder;
+///     long int *lp = malloc(sizeof(*lp));
+///     recorder.registerInstance(lp, free);
+///     FILE *fp;
+///     if (fp = fopen("data.txt", "rb")) {
+///         recorder.registerInstance(fp, (instance_destructor_func_t)fclose);
+///     }
+///
+///     // ...
+///
+///     // if (ErrorHappened()) {
+///     //     exit(255);
+///     // }
+///
+///     // ...
+///     return (0);
+/// }
+/// ```
+class ApplicationResourceRecorder {
+public:
+    void registerInstance(instance_ptr_t instance, instance_destructor_func_t destructorFunc);
+    ApplicationResourceRecorder();
+    ~ApplicationResourceRecorder();
+
+private:
+    std::stack<item_t> m_items;
+};
+
+#endif /* APPLICATION_RESOURCE_RECORDER_H */
