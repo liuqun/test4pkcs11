7f0afc3cdf2e6377e740797dab415fa2a7b2b5fb
Liu Qun (liuqun) <qunliu@zyhx-group.com>
2017-09-25 17:30:42 +0800

    new: test program written in C++

--- Makefile
+++ Makefile
@@ -2,9 +2,11 @@
 # Makefile for my sample program
 # https://github.com/opencryptoki/opencryptoki/blob/e460cc1ab72b3b27e648ff883b74bac0733c71af/doc/opencryptoki-howto.md#10-appendix-a-sample-program
 
+EXEC += test
 EXEC += print-current-tokens
 CUSTOMIZED_INCLUDE_DIRS =
 CFLAGS += -g -O0 $(CUSTOMIZED_INCLUDE_DIRS)
+CXXFLAGS += -g -O0 $(CUSTOMIZED_INCLUDE_DIRS)
 LIBS += -ldl -lpthread
 
 .PHONY: all
@@ -12,6 +14,15 @@ all: config.h $(EXEC)
 
 print-current-tokens: print-current-tokens.o pkcs11-api-loader.o
 	$(LINK.o) -o $@ $^ $(LIBS)
+
+test: test.o pkcs11-api-loader.o ApplicationResourceRecorder.o
+	$(LINK.o) -o $@ $^ $(LIBS) -lstdc++
+
+%.o: %.cpp %.h
+	$(COMPILE.cpp) -o $@ $<
+
+%.o: %.cpp
+	$(COMPILE.cpp) -o $@ $<
 
 %: %.c
 	$(LINK.c) -o $@ $^ $(LIBS)
---
+++ test.cpp
@@ -0,0 +1,134 @@
+﻿/* encoding:utf8 */
+
+/* Copyright (c) 2017, 青岛中怡智能安全研究院有限公司
+ * All rights reserved.
+ */
+
+#include <sys/stat.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <stdio.h>
+#include <dlfcn.h>
+#include "config.h"
+#ifndef HAVE_OPENCRYPTOKI_PKCS11_H
+#error // 找不到 opencryptoki/pkcs11.h
+#endif
+#include <opencryptoki/pkcs11.h>
+#include "ApplicationResourceRecorder.h"
+#include "pkcs11-api-loader.h"
+
+CK_RV print_slot_info(CK_SLOT_INFO slot_info);
+CK_RV print_token_info(CK_TOKEN_INFO token_info);
+
+int main(int argc, char *argv[])
+{
+    CK_FUNCTION_LIST_PTR function_ptr = NULL;
+    CK_RV rc;
+    pkcs11_api_t *api;
+    ApplicationResourceRecorder recorder;
+
+    api = new_pkcs11_api_instance("/usr/lib/opencryptoki/libopencryptoki.so");
+    if (!api) {
+        fprintf(stderr, "Error initializing the PKCS11 library\n");
+        exit(0xFF);
+    }
+    recorder.registerInstance((instance_ptr_t)api, (instance_destructor_func_t)delete_pkcs11_api_instance);
+
+    function_ptr = api->functions;
+
+    /* PKCS#11 library initialize */
+    rc = function_ptr->C_Initialize(NULL);
+    if (rc != CKR_OK) {
+        fprintf(stderr, "Error initializing the PKCS#11 library: rc=%X\n", (int)rc);
+        exit(0xFF);
+    }
+    recorder.registerInstance(NULL, (instance_destructor_func_t) function_ptr->C_Finalize);
+
+    do {
+        CK_ULONG slot_count = 0;
+
+        /* Find out how many tokens are present in the slots */
+        rc = function_ptr->C_GetSlotList(TRUE, NULL, &slot_count);
+        if (rc != CKR_OK) {
+            printf("Error getting number of slots: 0x%X\n", (int)rc);
+            break;
+        }
+        if (slot_count > 0) {
+            int i;
+            CK_SLOT_ID_PTR slot_list = NULL;
+
+            slot_list = (CK_SLOT_ID_PTR) calloc(slot_count, sizeof(CK_SLOT_ID));
+            recorder.registerInstance(slot_list, free);
+            rc = function_ptr->C_GetSlotList(TRUE, slot_list, &slot_count);
+
+            /* Display token info and slot info for each slot ID in "slot_list" */
+            for (i = 0; i < slot_count; i++) {
+                CK_RV token_info_err;
+                CK_RV slot_info_err;
+                CK_TOKEN_INFO token_info; ///< Structure to hold token information
+                CK_SLOT_INFO slot_info; ///< Structure to hold slot information
+                CK_SLOT_ID id;
+
+                id = slot_list[i];
+                token_info_err = function_ptr->C_GetTokenInfo(id, &token_info);
+                slot_info_err = function_ptr->C_GetSlotInfo(id, &slot_info);
+                if (token_info_err) {
+                    printf("Error getting token info: 0x%X\n", (int)token_info_err);
+                }
+                if (slot_info_err) {
+                    printf("Error getting the slot info: 0x%X\n", (int)slot_info_err);
+                }
+                if (token_info_err || slot_info_err) {
+                    continue;
+                }
+                printf("Token #%d Info:\n", (int)id);
+                print_token_info(token_info);
+                printf("Slot #%d Info\n", (int)id);
+                print_slot_info(slot_info);
+            }
+
+            slot_list = NULL;
+        }
+    } while (0);
+
+    return 0;
+}
+
+CK_RV print_slot_info(CK_SLOT_INFO slot_info)
+{
+    /* Display the slot information */
+    printf("\tDescription: %.64s\n", slot_info.slotDescription);
+    printf("\tManufacturer: %.32s\n", slot_info.manufacturerID);
+    printf("\tFlags: 0x%X\n", (int)slot_info.flags);
+    printf("\tHardware Version: %d.%d\n", slot_info.hardwareVersion.major,
+                                          slot_info.hardwareVersion.minor);
+    printf("\tFirmware Version: %d.%d\n", slot_info.firmwareVersion.major,
+                                          slot_info.firmwareVersion.minor);
+    return CKR_OK;
+}
+
+CK_RV print_token_info(CK_TOKEN_INFO token_info)
+{
+    /* Display the token information */
+    printf("\tLabel: %.32s\n", token_info.label);
+    printf("\tManufacturer: %.32s\n", token_info.manufacturerID);
+    printf("\tModel: %.16s\n", token_info.model);
+    printf("\tSerial Number: %.16s\n", token_info.serialNumber);
+    printf("\tFlags: 0x%X\n", (int)token_info.flags);
+    printf("\tSessions: %d/%d\n", (int)token_info.ulSessionCount,
+                                  (int)token_info.ulMaxSessionCount);
+    printf("\tR/W Sessions: %d/%d\n", (int)token_info.ulRwSessionCount,
+                                      (int)token_info.ulMaxRwSessionCount);
+    printf("\tPIN Length: %d-%d\n", (int)token_info.ulMinPinLen,
+                                    (int)token_info.ulMaxPinLen);
+    printf("\tPublic Memory: 0x%X/0x%X\n", (int)token_info.ulFreePublicMemory,
+                                           (int)token_info.ulTotalPublicMemory);
+    printf("\tPrivate Memory: 0x%X/0x%X\n", (int)token_info.ulFreePrivateMemory,
+                                           (int)token_info.ulTotalPrivateMemory);
+    printf("\tHardware Version: %d.%d\n", (int)token_info.hardwareVersion.major,
+                                          (int)token_info.hardwareVersion.minor);
+    printf("\tFirmware Version: %d.%d\n", token_info.firmwareVersion.major,
+                                          token_info.firmwareVersion.minor);
+    printf("\tTime: %.16s\n", token_info.utcTime);
+    return CKR_OK;
+}
