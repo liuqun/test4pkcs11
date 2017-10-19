50834f400e728d0b47206860f561e9e26fed4013
Liu Qun (liuqun) <qunliu@zyhx-group.com>
2017-10-19 20:34:23 +0800

    pkcs11-probe: 修改头文件使C++能调用C函数

--- pkcs11-probe.h
+++ pkcs11-probe.h
@@ -15,26 +15,33 @@
 
 typedef struct api_instance_t *api_t;
 
-api_t new_api_instance();
-void delete_api_instance(api_t instance);
-
 typedef union {
     void *ptr;
     struct api_instance_t *api;
     struct pkcs11_instance_t *pkcs11;
 } pkcs11_t;
 
-pkcs11_t new_pkcs11_instance();
-void delete_pkcs11_instance(pkcs11_t instance);
-const char *pkcs11_which_lib(pkcs11_t instance);
-const CK_FUNCTION_LIST_PTR pkcs11_get_api_function_list(pkcs11_t instance);
-
 typedef enum {
     PROBE_SUCCESS=0,
     PROBE_GENERIC_FAILURE,
 } probe_result_t;
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+api_t new_api_instance();
+void delete_api_instance(api_t instance);
+
+pkcs11_t new_pkcs11_instance();
+void delete_pkcs11_instance(pkcs11_t instance);
+const char *pkcs11_which_lib(pkcs11_t instance);
+const CK_FUNCTION_LIST_PTR pkcs11_get_api_function_list(pkcs11_t instance);
+
 probe_result_t api_probe(api_t instance, const char *lib);
 probe_result_t pkcs11_probe(pkcs11_t instance, const char *lib);
 
+#ifdef __cplusplus
+}; /* end of extern "C" */
+#endif
 #endif /* PKCS11_PROBE_H_ */
